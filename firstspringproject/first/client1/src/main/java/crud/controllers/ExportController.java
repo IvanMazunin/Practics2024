46fcdf31684c014990bb71992ed2f9e9beb8ceb3
Mazunin Ivan <mazuninim@gmail.com>
2024-06-17 22:39:01 +0500

    Change ExportService

--- firstspringproject/first/client1/src/main/java/crud/controllers/ExportController.java
+++ firstspringproject/first/client1/src/main/java/crud/controllers/ExportController.java
@@ -5,22 +5,17 @@ import java.io.IOException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import crud.exporters.FilesExporter;
 import crud.services.ExportService;
 
 @RestController
 @RequestMapping(path = "/api")
 public class ExportController {
-	private RestTemplate rest = new RestTemplate();
-
-	private FilesExporter export = new FilesExporter();
 
 	private ExportService service = new ExportService();
 
 	@GetMapping("/users/export/excel")
 	public void download() throws IOException{
-		export.exportToExcel(service.getUserDtos(rest));
+		service.exportToExcel();
 	};
 }
--- firstspringproject/first/client1/src/main/java/crud/services/ExportService.java
+++ firstspringproject/first/client1/src/main/java/crud/services/ExportService.java
@@ -1,5 +1,6 @@
 package crud.services;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
@@ -9,16 +10,22 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import crud.exporters.FilesExporter;
 import crud.service.dto.UserDto;
 import lombok.Data;
 
 @Data
 public class ExportService {
-	public List<UserDto> getUserDtos(RestTemplate rest){
+
+	private RestTemplate rest = new RestTemplate();
+
+	private FilesExporter exporter = new FilesExporter();
+
+	public void exportToExcel() throws IOException{
 		HttpEntity<UserDto> requestEntity = new HttpEntity<UserDto>(new HttpHeaders());
 		ParameterizedTypeReference<List<UserDto>> responseType = new ParameterizedTypeReference<List<UserDto>>() {};
 		ResponseEntity<List<UserDto>> resp = rest.exchange("http://localhost:9090/api/users", HttpMethod.GET, requestEntity, responseType);
 		var list = resp.getBody();
-		return list;
+		exporter.exportToExcel(list);
 	}
 }
